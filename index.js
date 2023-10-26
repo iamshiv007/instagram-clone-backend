d49bf032495418055a1ff751bdb504cb3bf18417
iamshiv007 <Github@#007>
2023-10-26 07:49:24 +0530

    asyncHandler, errorHandler, sendCookie, sendMail

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 require("dotenv").config()
 const cookieParser = require('cookie-parser');
+const cloudinary = require("cloudinary").v2
+const fileUpload = require("express-fileupload")
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(fileUpload())
+
+// Configuration 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
 
 // Connect to database
 const connectDb = require("./database/db")
@@ -20,3 +30,7 @@ app.listen(port, (req, res) => {
 app.get("/", (req, res) => {
     return res.send("Server testing: Hello from Instagram clone backend")
 })
+
+// error middleware
+const errorMiddleware  = require("./middleware/error")
+app.use(errorMiddleware);
